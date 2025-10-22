b07efa4a0a32b72a718844ad2abbb31de51c3809
Iván Fernández <ivan.fernandez@unl.edu.ec>
2025-10-22 17:45:05 -0500

    Corrección de Conflictos de nuevo

--- athletia/src/auth/auth.controller.ts
+++ athletia/src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import {
   Patch,
   Post,
   UseGuards,
+  Get, Req,
+  Res,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
--- athletia/src/workout/exercises/dto/exercises.dto.ts
+++ athletia/src/workout/exercises/dto/exercises.dto.ts
@@ -1,22 +1,15 @@
 import {
-<<<<<<< HEAD
-  IsArray,
-=======
   ArrayMinSize,
   IsArray,
   IsDate,
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
   IsEnum,
   IsNotEmpty,
   IsOptional,
   IsString,
   IsUrl,
-<<<<<<< HEAD
-=======
   IsUUID,
   MaxLength,
   MinLength,
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
 } from 'class-validator';
 import { MuscleTarget } from '../enum/muscle-target.enum';
 import { ExerciseType } from '../enum/exercise-type.enum';
@@ -24,20 +17,14 @@ import { ExerciseType } from '../enum/exercise-type.enum';
 export class ExerciseRequest {
   @IsString()
   @IsNotEmpty()
-<<<<<<< HEAD
-=======
   @MinLength(3)
   @MaxLength(50)
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
   name: string;
 
   @IsString()
   @IsNotEmpty()
-<<<<<<< HEAD
-=======
   @MinLength(10)
   @MaxLength(500)
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
   description: string;
 
   @IsUrl()
@@ -47,35 +34,23 @@ export class ExerciseRequest {
   @IsArray()
   @IsEnum(MuscleTarget, {
     each: true,
-<<<<<<< HEAD
-    message: `Each muscleTarget must be one of: ${Object.values(MuscleTarget).join(', ')}`,
-  })
-  @IsNotEmpty()
-=======
     message: `Each muscleTarget must be one of: ${Object.values(
       MuscleTarget,
     ).join(', ')}`,
   })
   @IsNotEmpty()
   @ArrayMinSize(1)
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
   muscleTarget: MuscleTarget[];
 
   @IsArray()
   @IsEnum(ExerciseType, {
     each: true,
-<<<<<<< HEAD
-    message: `Each exerciseType must be one of: ${Object.values(ExerciseType).join(', ')}`,
-  })
-  @IsNotEmpty()
-=======
     message: `Each exerciseType must be one of: ${Object.values(
       ExerciseType,
     ).join(', ')}`,
   })
   @IsNotEmpty()
   @ArrayMinSize(1)
->>>>>>> 7cd40a95bddcc5373169e9df425d6b320fab7f64
   exerciseType: ExerciseType[];
 }
 
